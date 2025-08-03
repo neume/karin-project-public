fb69da64a542f806d3c653d65443d1f9901b3c52
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2025-08-04 00:12:33 +0800

    Add PhysicsBodyComponent

---
+++ Karin/Components/PhysicsBodyComponent.cs
@@ -0,0 +1,17 @@
+using Karin.GuiTools;
+using Microsoft.Xna.Framework;
+
+[ToolInspectable]
+public struct PhysicsBodyComponent
+{
+    [ToolSerializeField("Body", false)]
+    public string Body;
+
+    [ToolSerializeField("Is Kinematic")]
+    public bool IsKinematic;
+
+    [ToolSerializeField("Mass")]
+    public float Mass;
+
+    [ToolSerializeField("Velocity")]
+    public Vector2 Velocity; }
