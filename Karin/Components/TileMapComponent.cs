ae41fe97307e70441563e07ddc7e5334333f3a65
Joseph Nelson Valeros <valerosjoseph@gmail.com>
2025-08-03 23:59:04 +0800

    Remove unused fields from TileMapComponent

--- Karin/Components/TileMapComponent.cs
+++ Karin/Components/TileMapComponent.cs
@@ -9,17 +9,18 @@ public struct TileMapComponent
     public Dictionary<Vector2, int> Map;
     public Dictionary<int, Rectangle> TextureStore;
     public string TextureName;
-    public Vector2 Position;
+
     [ToolSerializeField("Visible")]
     public bool IsVisible;
+
     [ToolSerializeField("Tile Width")]
     public int TileWidth;
+
     [ToolSerializeField("Tile Height")]
     public int TileHeight;
 
     public TileMapComponent(){
         IsVisible = false;
-        Position = new Vector2(0, 0);
         TileWidth = 32;
         TileHeight = 32;
         TextureStore = new Dictionary<int, Rectangle>();
